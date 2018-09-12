21641a53461a242e56ef077bed10f58752842350
Son <>
2018-09-12 19:54:58 +0700

    no message

---
+++ LifeCycle/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  LifeCycle
+//
+//  Created by Son on 8/8/18.
+//  Copyright © 2018 NguyenHoangSon. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("\(type(of: self)) - viewDidLoad")
+    }
+    
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("\(type(of: self)) - viewWillAppear")
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("\(type(of: self)) - viewDidAppear")
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("\(type(of: self)) - viewWillDisappear")
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("\(type(of: self)) - viewDidDisappear")
+    }
+    deinit {
+        print("\(type(of: self)) - Deinit")
+    }
+
+}
